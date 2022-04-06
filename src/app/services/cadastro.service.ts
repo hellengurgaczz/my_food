995b3fe06505ebbb6b2c530d1495e853cc2eeda1
hellen - up <Aluno@DOSO027392.educacional.up>
2022-04-06 20:23:37 -0300

    add

--- src/app/services/cadastro.service.ts
+++ src/app/services/cadastro.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { Produto } from "../models/produto.model";
 
 @Injectable({
@@ -8,8 +9,25 @@ import { Produto } from "../models/produto.model";
 
 export class CadastroService {
     constructor(private http: HttpClient){}
+    URL = "http://localhost:3000";
 
-    listarProdutos() {
-        return this.http.get<[Produto]>('http://localhost:3000/pessoas')
+    listarProdutos(): Observable<Produto[]> {
+        return this.http.get<[Produto]>(this.URL + '/pessoas')
+    }
+
+    buscarProdutoPorId(id: number): Observable<Produto[]> {
+        return this.http.get<[Produto]>(this.URL + "/" + id)
+    }
+
+    IncluirProduto(produto: Produto) {
+        return this.http.post(this.URL, produto)
+    }
+
+    atualizarProduto(produto: Produto, id: number) {
+        return this.http.put(this.URL + "/" +  id, produto)
+    }
+
+    deletarProduto(id: number) {
+        return this.http.delete(this.URL + "/" +  id)
     }
 }
